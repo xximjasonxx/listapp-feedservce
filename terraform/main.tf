27c553cd72a62c62d853bf94555a6e22fc296b40
Jason Farrell <imjason@gmail.com>
2019-03-24 16:11:45 -0500

    Updated Ignore, added initial TF document

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+    version = "=1.22.0"
+    subscription_id = "cd46f7e3-6631-46d9-990c-6d18ee3ba8ef"
+}
+
+variable "container_id" {
+  type = "string"
+}
+
+data "azurerm_resource_group" "general" {
+  name = "ListApp-General"
+}
+
+data "azurerm_container_registry" "general" {
+  name = "ListAppRegistry"
+  resource_group_name = "${data.azurerm_resource_group.general.name}"
+}
+
+data "azurerm_app_service_plan" "general" {
+  name = "PreProd-ConsumptionPlan"
+  resource_group_name = "${data.azurerm_resource_group.general.name}"
+}
+
+resource "azurerm_resource_group" "feed" {
+    name = "ListApp-FeedService"
+    location = "Central  US"
+}
+
+resource "azurerm_app_service" "feed" {
+  name                = "feedservice-dev"
+  location            = "${azurerm_resource_group.feed.location}"
+  resource_group_name = "${azurerm_resource_group.feed.name}"
+  app_service_plan_id = "${data.azurerm_app_service_plan.general.id}"
+
+  app_settings {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    DOCKER_REGISTRY_SERVER_URL = "${data.azurerm_container_registry.general.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = "${data.azurerm_container_registry.general.admin_username}"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "${data.azurerm_container_registry.general.admin_password}"
+    ASPNETCORE_ENVIRONMENT = "Development"
+  }
+
+  site_config {
+    linux_fx_version = "DOCKER|${data.azurerm_container_registry.general.login_server}/feedservice:${var.container_id}"
+    always_on = "true"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
